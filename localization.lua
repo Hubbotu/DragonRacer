8bfb35c8dd2570a3371d1582478441163cc12a5c
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-03-13 07:04:27 +0300

    Add files via upload

---
+++ localization.lua
@@ -0,0 +1,26 @@
+DgRacer.vars["localTbl"] = {}
+
+if (GetLocale() == "ruRU") then
+	DgRacer.vars["localTbl"]["Display available commands"] = "Показать доступные команды";
+	DgRacer.vars["localTbl"]["Cannot be loaded is this WoW version."] = "Не может быть загружен в этой версии WoW.";
+	DgRacer.vars["localTbl"]["Always use a smaller flight interface"] = "Всегда использовать уменьшенный интерфейс полета";
+	DgRacer.vars["localTbl"]["Enable interface auto-hide when racing"] = "Включить автоматическое скрытие интерфейса во время гонки";
+	DgRacer.vars["localTbl"]["Prefer interface transparency"] = "Предпочитать прозрачность интерфейса";
+	DgRacer.vars["localTbl"]["Right-click vigor frame to display options."] = "ПКМ на рамке энергии, чтобы отобразить опции.";
+	DgRacer.vars["localTbl"]["Restart race"] = "Перезапустить гонку";
+	DgRacer.vars["localTbl"]["Available commands:"] = "Доступные команды:";
+	DgRacer.vars["localTbl"]["Enlarge flight interface when racing"] = "Увеличить интерфейс полета во время гонки";
+	DgRacer.vars["localTbl"]["Loaded"] = "Загружен";
+	DgRacer.vars["localTbl"]["You must be racing to reset race."] = "Вы должны быть в гонке, чтобы перезапустить гонку.";
+	DgRacer.vars["localTbl"]["Reset all WoW interface settings"] = "Сбросить все настройки интерфейса WoW";
+	DgRacer.vars["localTbl"]["Deactivate addon"] = "Деактивировать аддон";
+	DgRacer.vars["localTbl"]["Enter /dragonracer to activate addon."] = "Введите /dragonracer, чтобы активировать аддон.";
+	DgRacer.vars["localTbl"]["OK"] = "ОК";
+	DgRacer.vars["localTbl"]["Not activated."] = "Не активирован.";
+	DgRacer.vars["localTbl"]["deactivated."] = "деактивирован.";
+	DgRacer.vars["localTbl"]["display current addon settings"] = "показать текущие настройки аддона";
+	DgRacer.vars["localTbl"]["Activate/deactivate addon"] = "Активировать/деактивировать аддон";
+	DgRacer.vars["localTbl"]["Show spells when racing"] = "Показывать заклинания во время гонки";
+	DgRacer.vars["localTbl"]["Always use a larger flight interface"] = "Всегда использовать увеличенный интерфейс полета";
+	DgRacer.vars["localTbl"]["Force main WoW interface to be shown"] = "Принудительно отображать основной интерфейс WoW";
+end
